66926b6550bfad26977d33c61e91a9815abf81d4
hamza-25 <me677499@gmail.com>
2024-03-20 15:27:05 +0000

    update

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- script that creates a table users
--- id, email, name, ENUM
+-- id, email, name, country
 CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email varchar(255) NOT NULL UNIQUE,
-    name varchar(255),
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
 )
