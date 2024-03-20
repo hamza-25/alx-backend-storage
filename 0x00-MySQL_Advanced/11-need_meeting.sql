6af14c7d28fc1ccf9e7322bbd3bfaab47e99aae7
hamza-25 <me677499@gmail.com>
2024-03-20 21:11:45 +0000

    update

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,10 +1,5 @@
 -- script that creates a view need_meeting that lists all students that have a score under 80 (strict)
 -- and no last_meeting or more than 1 month.
-DILIMETER $$ ;
-CREATE VIEW AS 
-BEGIN
-    SELECT name FROM students WHERE score < 80 AND (
-        last_meeting = NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH)
+CREATE VIEW AS SELECT name FROM students WHERE score < 80 AND (
+    last_meeting = NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH)
     );
-END;$$
-DILIMETER ;
