1e594131e5258fb289b2d34dac0471ecd2491b7c
hamza-25 <me677499@gmail.com>
2024-03-25 18:41:39 +0000

    update

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,45 +3,18 @@
 import pymongo
 
 
-def get_logs_stats(mongo_collection, option=None):
-    """
-    Provides some stats about Nginx logs stored in MongoDB
-    Prototype: def get_logs_stats(mongo_collection, option=None)
-    """
+def get_logs_stats(mongo_collection):
+    """ provides some stats about Nginx logs stored in MongoDB"""
     methods = {'GET': 0, 'POST': 0, 'PUT': 0, 'PATCH': 0, 'DELETE': 0}
-    status_check = 0
-
-    # Count all logs
-    count = mongo_collection.count_documents({})
-
-    # If option is provided, count only logs matching the specified method
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    # Count logs for each method
     results = mongo_collection.find()
+    count = 0
+    status_check = 0
     for obj in results:
+        count += 1
         if obj['method'] in methods.keys():
             methods[obj['method']] += 1
-        if obj['path'] == '/status':
+        if obj['path'] == '/status' and obj['method'] == 'GET':
             status_check += 1
-
-# ## 2 func
-# def get_logs_stats(mongo_collection):
-#     """ provides some stats about Nginx logs stored in MongoDB"""
-#     methods = {'GET': 0, 'POST': 0, 'PUT': 0, 'PATCH': 0, 'DELETE': 0}
-#     results = mongo_collection.find()
-#     count = 0
-#     status = 0
-#     for obj in results:
-#         count += 1
-#         if obj['method'] in methods.keys():
-#             methods[obj['method']] += 1
-#         if obj['path'] == '/status':
-#             status += 1
     print("{} logs \n\tmethod GET: {}\n\tmethod POST: {}\n\tmethod PUT: {}\n\t\
 method PATCH: {}\n\tmethod DELETE: {}\n{} status check"
           .format(count, methods["GET"], methods["POST"],
