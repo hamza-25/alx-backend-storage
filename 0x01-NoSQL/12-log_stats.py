c6ba30230a6bada15496b859180eee25c328ff8e
hamza-25 <me677499@gmail.com>
2024-03-25 18:13:08 +0000

    update

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,13 +3,10 @@
 import pymongo
 
 
-def get_logs_stats():
+def get_logs_stats(mongo_collection):
     """ provides some stats about Nginx logs stored in MongoDB"""
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client['logs']
-    collection = db['nginx']
     methods = {'GET': 0, 'POST': 0, 'PUT': 0, 'PATCH': 0, 'DELETE': 0}
-    results = collection.find()
+    results = mongo_collection.find()
     count = 0
     status = 0
     for obj in results:
@@ -18,10 +15,14 @@ def get_logs_stats():
             methods[obj['method']] += 1
         if obj['path'] == '/status':
             status += 1
-    print("{} logs \n\tmethod GET: {}\n\tmethod POST: {}\n\tmethod PUT: {}\n\tmethod PATCH: {}\n\tmethod DELETE: {}\n{} status check"
+    print("{} logs \n\tmethod GET: {}\n\tmethod POST: {}\n\tmethod PUT: {}\n\t\
+        method PATCH: {}\n\tmethod DELETE: {}\n{} status check"
           .format(count, methods["GET"], methods["POST"],
                   methods["PUT"], methods["PATCH"], methods["DELETE"], status))
 
 
 if __name__ == '__main__':
-    get_logs_stats()
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client['logs']
+    collection = db['nginx']
+    get_logs_stats(collection)
