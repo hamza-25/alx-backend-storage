a4817b005a7c7db2d80dcc06f53f162e8968b482
hamza-25 <me677499@gmail.com>
2024-03-27 12:03:53 +0000

    create instance of redis and method store

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Define Cache class
+"""
+import redis
+from typing import Union
+from uuid import uuid1
+
+
+class Cache:
+    """Cache class"""
+    def __init__(self) -> None:
+        """store an instance of the Redis client"""
+        self._redis = redis.Redis()
+
+    def store(self, data: Union[str, float, bytes, int]) -> str:
+        """return id as string and set on redis data"""
+        id = str(uuid1())
+        self._redis.set(id, data)
+        return id
