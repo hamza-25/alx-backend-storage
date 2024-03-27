be500560d4c21dc19d9f8ea4fa4b1ee7ac2f200e
hamza-25 <me677499@gmail.com>
2024-03-27 12:15:03 +0000

    up

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-"""Define Cache class
+"""Module defines class and methods redis
 """
 import redis
 from typing import Union
-from uuid import uuid1
+from uuid import uuid4
 
 
 class Cache:
-    """Cache class"""
+    """declares Cache redis class"""
     def __init__(self) -> None:
         """store an instance of the Redis client"""
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
+        self._redis.flushdb()
 
     def store(self, data: Union[str, float, bytes, int]) -> str:
         """return id as string and set on redis data"""
-        id = str(uuid1())
-        self.__redis.set(id, data)
+        id = str(uuid4())
+        self._redis.set(id, data)
         return id
